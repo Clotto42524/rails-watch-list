40af5948bd80d09453008481c7a789336d5e90bf
Clyde Choi <clydechoi@gmail.com>
2023-02-23 14:49:18 +0100

    functionality done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, presence: true, uniqueness: { scope: :list_id}
-  validates :list_id, presence: true, uniqueness: { scope: :movie_id}
+  validates :list, presence: true, uniqueness: { scope: :movie }
   validates :comment, length: { minimum: 6 }
 end
